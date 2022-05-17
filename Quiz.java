a3c0a9c20a9c635ba07b8587f50809a50685ebe5
MayyarJavaProgrammer <mayarzidane1@gmail.com>
2022-05-17 11:10:33 +0300

    Add showQuestionMessage method

---
+++ Quiz.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mear
+ */
+public class Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int showQuestionMessage() {
+        System.out.println("Please enter the maximum number of questions");
+        return reader.nextInt();
+
+    }
+
+}
