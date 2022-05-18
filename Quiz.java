aa6673059688e74f4502f7e13419f02373a76588
MayyarJavaProgrammer <mayarzidane1@gmail.com>
2022-05-18 11:57:55 +0300

    Add generateRandomString method

--- Quiz.java
+++ Quiz.java
@@ -4,6 +4,7 @@
  */
 package quiz;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -20,27 +21,42 @@ public class Quiz {
     public static void main(String[] args) {
         int numberOfQuestions = showNumberQuestionMessage();
         showMargins();
-        getNumberOfCharacters();
+        char randomString[];
+        randomString = generateRandomString(getNumberOfCharacters());
     }
 
     public static int showNumberQuestionMessage() {
         System.out.println("Please enter the maximum number of questions");
-        return reader.nextInt();
-
+            int number = reader.nextInt();
     }
 
     public static void showMargins() {
         System.out.println("==================================");
     }
-    
-    public static int getNumberOfCharacters() {
-            System.out.println("Please enter an integer value between 3 and 100 (the number of characters from which to enumerate certain (Odd/Even?Primary) numbers -- Degree of difficulty)");
-            int numberOfCharacters = reader.nextInt();
-            if(numberOfCharacters >= 3 || numberOfCharacters <= 100) {
-                return numberOfCharacters;
-            } else {
-                return getNumberOfCharacters();
-            }          
-    } 
 
+    public static int getNumberOfCharacters() {
+        System.out.println("Please enter an integer value between 3 and 100 (the number of characters from which to enumerate certain (Odd/Even?Primary) numbers -- Degree of difficulty)");
+        int numberOfCharacters = reader.nextInt();
+        if (numberOfCharacters >= 3 && numberOfCharacters <= 100) {
+            return numberOfCharacters;
+        } else {
+            return getNumberOfCharacters();
+        }
+    }
+
+    public static char[] generateRandomString(int numberOfCharacters) {
+        String allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+        int allCharLength = allCharacters.length();
+        String RandomString = "";
+        for (int i = 1; i <= numberOfCharacters; i++) {
+            //get random index from allCharacters
+            int index = (int) (Math.random() * (allCharLength - 0) + 0);
+            //get char from random index and set in string 
+            RandomString += allCharacters.charAt(index);
+        }
+        //convert RandomString to char array and return it
+        return RandomString.toCharArray();
+    }
 }
