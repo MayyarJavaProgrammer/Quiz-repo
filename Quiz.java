d560c0d429a2a43b14d875a6f94228fee042a909
MayyarJavaProgrammer <mayarzidane1@gmail.com>
2022-05-22 17:22:43 +0300

    add calculateNumbers feature

--- Quiz.java
+++ Quiz.java
@@ -4,6 +4,7 @@
  */
 package quiz;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
@@ -19,12 +20,26 @@ public class Quiz {
      */
     static Scanner reader = new Scanner(System.in);
     static int numberOfQuestions;
-    static char[] randomString;
+    static String[] randomStrings;
+    static String[] typeMathNumbers = {"Primary", "Odd", "Even"};
+    static int[] typeOfNumberQuestion;
+    static int[] userAnswers;
+    static int[] checkUserAnswers;
+    static int[] correctِِِِِِِAnswers;
+    static int numberOfCharacters;
+    static int counterNumber = 0;
+    
     public static void main(String[] args) {
         numberOfQuestions = showNumberQuestionMessage();
+
+        randomStrings = new String[numberOfQuestions];
+        typeOfNumberQuestion = new int[numberOfQuestions];
+        userAnswers = new int[numberOfQuestions];
+        correctِِِِِِِAnswers = new int[numberOfQuestions];
+        checkUserAnswers = new int[numberOfQuestions];
         showMargins();
-        randomString = generateRandomString(getNumberOfCharacters());
-        showQuestionMessage(randomString);
+        numberOfCharacters = getNumberOfCharacters();
+        showQuestionMessage();
     }
 
     public static int showNumberQuestionMessage() {
@@ -65,7 +80,7 @@ public class Quiz {
         }
     }
 
-    public static char[] generateRandomString(int numberOfCharacters) {
+    public static String generateRandomString(int numberOfCurrentQuestion) {
         String allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                 + "0123456789"
                 + "abcdefghijklmnopqrstuvxyz";
@@ -74,35 +89,99 @@ public class Quiz {
         for (int i = 1; i <= numberOfCharacters; i++) {
             //get random index from allCharacters
             int index = (int) (Math.random() * (allCharLength - 0) + 0);
+            char c = allCharacters.charAt(index);
             //get char from random index and set in string 
-            RandomString += allCharacters.charAt(index);
+            RandomString += c;
+            if (Character.isDigit(c)) {
+                calculateNumbers(c, numberOfCurrentQuestion);
+            }
         }
-        //convert RandomString to char array and return it
-        return RandomString.toCharArray();
+        correctِِِِِِِAnswers[numberOfCurrentQuestion] = counterNumber;
+        counterNumber = 0;
+        return RandomString;
     }
 
-    public static void showQuestionMessage(char[] randomString) {
-        System.out.printf("How many times %s numbers appear in the following characters:%n",
-                getTypeNumbers());
-        System.out.println(randomString);
-        try {
-            reader.nextInt();
-        } catch (InputMismatchException ex) {
-            reader = new Scanner(System.in);
-            showQuestionMessage(randomString);
-        } catch (Exception ex) {
-            showQuestionMessage(randomString);
+    public static void showQuestionMessage() {
+        int i = 0;
+        int index = 0;
+        while (i < numberOfQuestions) {
+            if (randomStrings[i] == null && typeOfNumberQuestion[i] == 0) {
+                index = new Random().nextInt(3);
+                randomStrings[i] = generateRandomString(i);
+                typeOfNumberQuestion[i] = index;
+            }
+            System.out.printf("How many times %s numbers appear in the following characters:%n %s %n To ignore the question type ignore%n",
+                    typeMathNumbers[index], randomStrings[i]);
+
+            try {
+                String userAnswer = reader.next();
+                if (userAnswer.equalsIgnoreCase("ignore")) {
+                    userAnswers[i] = 0;
+                    System.out.println("Ignored");
+                } else {
+                    userAnswers[i] = Integer.parseInt(userAnswer);
+                }
+                checkAnswers(i);
+            } catch (InputMismatchException ex) {
+                //to avoid calls forever
+                reader = new Scanner(System.in);
+                continue;
+            } catch (Exception ex) {
+                continue;
+            }
+            i++;
+
         }
     }
 
-    public static String getTypeNumbers() {
-        Random r = new Random();
-        //get all types of numbers
-        String[] typeNumbers = {"Odd", "Even", "Primary"};
-        //get index between 0 and 2
-        int index = r.nextInt(3);
-        //return type of number 
-        return typeNumbers[index];
+    public static void calculateNumbers(char c, int numberOfCurrentQuestion) {
+        int typeNumber = typeOfNumberQuestion[numberOfCurrentQuestion];
+        // - '0' because we will get ascii value when we do this.. we will get the real number
+        int number = (c - '0');
+        //even type
+        switch (typeNumber) {
+            case 2 -> {
+                if (number % 2 == 0) {
+                    counterNumber++;
+                }
+            }
+            case 1 -> {
+                if (number % 2 != 0) {
+                    counterNumber++;
+                }
+            }
+            default -> {
+                if (isPrimary(number)) {
+                    counterNumber++;
+                }
+            }
+        }
     }
 
+    public static boolean isPrimary(int number) {
+        if (number == 2 || number == 3) {
+            return true;
+        } //if number is even or 1 it is not primary
+        else if (number % 2 == 0 || number == 1) {
+            return false;
+        } else {
+            //if number % anyOddNumber == 0 is not primary.. 
+            //if i == number or i > number that mean the number is 3 or 2 so do'nt loop
+            for (int i = 3; i < 9; i += 2) {
+                if (number % i == 0 && number != i) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void checkAnswers(int numberOfCurrentQuestion) {
+        if (userAnswers[numberOfCurrentQuestion] == correctِِِِِِِAnswers[numberOfCurrentQuestion]) {
+            checkUserAnswers[numberOfCurrentQuestion] = 1;
+        } else {
+            checkUserAnswers[numberOfCurrentQuestion] = 0;
+        }
+
+    }
 }
